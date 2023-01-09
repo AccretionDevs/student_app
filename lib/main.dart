3f5664de5f7217134772071484ac5f2c6e771a4a
Usaid Aijaz <malikusaid10@gmail.com>
2023-01-09 21:39:01 +0530

    fixed logo path

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:student_app/login.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
-    routes: {'login': (context) => const MyLogin()},
-  ));
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'login',
+        routes: {'login': (context) => const MyLogin()},
+      ));
 }
