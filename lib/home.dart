741fe138dae4eed1582146fe959249ec186ac08e
Usaid Aijaz <malikusaid10@gmail.com>
2023-01-11 00:18:04 +0530

    Add home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:developer';
 
 void main() {
   runApp(const HomePage());
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Home Screen',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Student Dairy'),
-        ),
         body: FutureBuilder(
             future: SharedPreferences.getInstance(),
             builder: (context, snapshot) {
@@ -28,16 +32,127 @@ class HomePage extends StatelessWidget {
               final prefs = snapshot.data;
               final name = prefs?.getString('name');
 
-              return Center(
-                child: Text(
-                  'Welcome to Student Dairy, $name',
+              return Scaffold(
+                body: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.13,
+                          ),
+                          const CircleAvatar(
+                            radius: 60,
+                            backgroundImage:
+                                AssetImage('assets/images/logo.png'),
+                            backgroundColor: Colors.transparent,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    Text(
+                      name.toString().toUpperCase(),
+                      style: TextStyle(
+                        // color: Colors.grey,
+                        fontFamily: 'Quicksand',
+                        fontSize: 26,
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    Card(
+                      // margin: EdgeInsets.symmetric(horizontal: 4, vertical: 40.0),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            top: 20, bottom: 20, left: 20, right: 180),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Registration Number:",
+                                style: TextStyle(fontSize: 12.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.01),
+                            Text("2020NITSGRxyz",
+                                style: TextStyle(fontSize: 18.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02),
+                            Text("Branch:", style: TextStyle(fontSize: 12.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.01),
+                            Text("pz", style: TextStyle(fontSize: 18.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02),
+                            Text("Enrollment Number:",
+                                style: TextStyle(fontSize: 12.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.01),
+                            Text("2020BCSEz", style: TextStyle(fontSize: 18.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02),
+                            Text("Semester:", style: TextStyle(fontSize: 12.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.01),
+                            Text("pz", style: TextStyle(fontSize: 18.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02),
+                            Text("CGPA:", style: TextStyle(fontSize: 12.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.01),
+                            Text("pz", style: TextStyle(fontSize: 18.0)),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Home',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.assessment),
+                      label: 'Result',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.person),
+                      label: 'Info',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.settings),
+                      label: 'Settings',
+                    ),
+                  ],
+                  iconSize: 35,
+                  currentIndex: _selectedIndex,
+                  onTap: _onItemTapped,
                 ),
               );
             }),
-        // body: const Center(
-        //   child: Text('Welcome to Student Dairy'),
-        // ),
       ),
     );
   }
+}
+
+void _onItemTapped(int index) {
+  log('$index');
 }
