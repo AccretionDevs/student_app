1db48c65adc59ceb9a673737da9e670d94e52caf
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2023-01-10 21:20:07 +0530

    Remove Debug Info

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -205,7 +204,6 @@ Future<http.Response> login(String re, String ps, bool rp) async {
       throw Exception("Invalid Username or Password");
     }
   } catch (error) {
-    log(error.toString());
     rethrow;
   }
 }
