9a19983b47531a9aa1968bf5ec402d2a4e9030ff
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2023-01-21 03:09:39 +0530

    Fetch Student Info, Fee Info

--- lib/functions/fetch.dart
+++ lib/functions/fetch.dart
@@ -1,8 +1,5 @@
-import 'dart:developer';
-
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 Future<void> fetchData() async {
   // await Future.delayed(const Duration(milliseconds: 1000));
@@ -10,8 +7,8 @@ Future<void> fetchData() async {
   final prefs = await SharedPreferences.getInstance();
 
   final token = prefs.getString('token') ?? "NA";
-  // final regno = prefs.getString('regno') ?? "NA";
-  // final enroll = prefs.getString('enroll') ?? "NA";
+  final regno = prefs.getString('regno') ?? "NA";
+  final enroll = prefs.getString('enroll') ?? "NA";
   final uatype = prefs.getString('uatype') ?? "NA";
   final uano = prefs.getString('uano') ?? "NA";
   // final hfid = prefs.getString('hfid') ?? "NA";
@@ -34,15 +31,62 @@ Future<void> fetchData() async {
   final responseImage =
       http.post(urlImage, body: dataImage, headers: headersImage);
 
-  // Uri urlInfo = Uri.parse('https://android.nitsri.ac.in/api/v2/information/stud');
+  Uri urlInfo =
+      Uri.parse('https://android.nitsri.ac.in/api/v2/information/stud');
+  Map<String, String> headersInfo = {
+    "uatype": uatype,
+    "id": uano,
+    "token": token,
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "android.nitsri.ac.in",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/5.0.0-alpha.2"
+  };
+  Map<String, String> dataInfo = {"id": enroll};
+  String dataInfoLen = Uri(queryParameters: dataInfo).query.length.toString();
+  headersInfo["Content-Length"] = dataInfoLen;
+  final responseInfo = http.post(urlInfo, body: dataInfo, headers: headersInfo);
+
   // Uri urlResultInt = Uri.parse('https://android.nitsri.ac.in/api/v2/exam/int');
   // Uri urlResultExt = Uri.parse('https://android.nitsri.ac.in/api/v2/exam/ext');
-  // Uri urlFees = Uri.parse('https://android.nitsri.ac.in/api/v2/fees/paid');
+
+  Uri urlFees = Uri.parse('https://android.nitsri.ac.in/api/v2/fees/paid');
+  Map<String, String> headersFees = {
+    "uatype": uatype,
+    "id": uano,
+    "token": token,
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "android.nitsri.ac.in",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/5.0.0-alpha.2"
+  };
+  Map<String, String> dataFees = {"id": regno};
+  String dataFeesLen = Uri(queryParameters: dataFees).query.length.toString();
+  headersFees["Content-Length"] = dataFeesLen;
+  final responseFees = http.post(urlFees, body: dataFees, headers: headersFees);
 
   await responseImage
       .then((response) async => {
             if (response.statusCode == 200)
               {await prefs.setString('image_info', response.body)},
+          })
+      .catchError((error) => {
+            {true}
+          });
+  await responseInfo
+      .then((response) async => {
+            if (response.statusCode == 200)
+              {await prefs.setString('student_info', response.body)},
+          })
+      .catchError((error) => {
+            {true}
+          });
+  await responseFees
+      .then((response) async => {
+            if (response.statusCode == 200)
+              {await prefs.setString('fees_info', response.body)},
           })
       .catchError((error) => {
             {true}
