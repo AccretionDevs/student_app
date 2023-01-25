f6935cd2ae8b22aecca6c8049429147f56bd1840
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2023-01-26 02:32:37 +0530

    Fetch Result

--- lib/functions/fetch.dart
+++ lib/functions/fetch.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,6 +13,9 @@ Future<void> fetchData() async {
   final uatype = prefs.getString('uatype') ?? "NA";
   final uano = prefs.getString('uano') ?? "NA";
   // final hfid = prefs.getString('hfid') ?? "NA";
+
+  final loginInfoJson = prefs.getString('login_info') ?? "{}";
+  Map<String, dynamic> loginInfo = jsonDecode(loginInfoJson);
 
   Uri urlImage =
       Uri.parse('https://android.nitsri.ac.in/api/v2/information/img');
@@ -48,8 +52,57 @@ Future<void> fetchData() async {
   headersInfo["Content-Length"] = dataInfoLen;
   final responseInfo = http.post(urlInfo, body: dataInfo, headers: headersInfo);
 
-  // Uri urlResultInt = Uri.parse('https://android.nitsri.ac.in/api/v2/exam/int');
-  // Uri urlResultExt = Uri.parse('https://android.nitsri.ac.in/api/v2/exam/ext');
+  Uri urlResultInt = Uri.parse('https://android.nitsri.ac.in/api/v2/exam/int');
+  Map<String, String> headersResultInt = {
+    "uatype": uatype,
+    "id": uano,
+    "token": token,
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "android.nitsri.ac.in",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/5.0.0-alpha.2"
+  };
+  List<Future<http.Response>> responseResultInt = [];
+  for (int i = 0; i < loginInfo["InternalSession"].length; i++) {
+    Map<String, String> dataResultInt = {
+      "id": enroll,
+      "no": loginInfo["InternalSession"]?[i]?["SessionNo"] ?? "0",
+      "sem": loginInfo["InternalSession"]?[i]?["SemesterNo"] ?? "0"
+    };
+    String dataResultIntLen =
+        Uri(queryParameters: dataResultInt).query.length.toString();
+    headersInfo["Content-Length"] = dataResultIntLen;
+    final resp =
+        http.post(urlResultInt, body: dataResultInt, headers: headersResultInt);
+    responseResultInt.add(resp);
+  }
+
+  Uri urlResultExt = Uri.parse('https://android.nitsri.ac.in/api/v2/exam/ext');
+  Map<String, String> headersResultExt = {
+    "uatype": uatype,
+    "id": uano,
+    "token": token,
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "android.nitsri.ac.in",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/5.0.0-alpha.2"
+  };
+  List<Future<http.Response>> responseResultExt = [];
+  for (int i = 0; i < loginInfo["ExternalSession"].length; i++) {
+    Map<String, String> dataResultExt = {
+      "id": enroll,
+      "no": loginInfo["ExternalSession"]?[i]?["SessionNo"] ?? "0",
+      "sem": loginInfo["ExternalSession"]?[i]?["SemesterNo"] ?? "0"
+    };
+    String dataResultExtLen =
+        Uri(queryParameters: dataResultExt).query.length.toString();
+    headersInfo["Content-Length"] = dataResultExtLen;
+    final resp =
+        http.post(urlResultExt, body: dataResultExt, headers: headersResultExt);
+    responseResultExt.add(resp);
+  }
 
   Uri urlFees = Uri.parse('https://android.nitsri.ac.in/api/v2/fees/paid');
   Map<String, String> headersFees = {
@@ -91,4 +144,30 @@ Future<void> fetchData() async {
       .catchError((error) => {
             {true}
           });
+
+  List<dynamic> listResultInt = [];
+  for (Future<http.Response> resp in responseResultInt) {
+    await resp
+        .then((response) async => {
+              if (response.statusCode == 200)
+                {listResultInt.add(jsonDecode(response.body))},
+            })
+        .catchError((error) => {
+              {true}
+            });
+  }
+  await prefs.setString('res_int', jsonEncode(listResultInt));
+
+  List<dynamic> listResultExt = [];
+  for (Future<http.Response> resp in responseResultExt) {
+    await resp
+        .then((response) async => {
+              if (response.statusCode == 200)
+                {listResultExt.add(jsonDecode(response.body))},
+            })
+        .catchError((error) => {
+              {true}
+            });
+  }
+  await prefs.setString('res_ext', jsonEncode(listResultExt));
 }
